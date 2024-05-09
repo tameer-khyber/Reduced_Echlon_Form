8ea4870ca65ecbdbe80612235eeb072405435692
usaid-khan-chandio <usiadkhanchandio@gmail.com>
2024-05-09 13:52:22 +0500

    Added object of Steps & logic class and declared the matrix as well commented input for now

--- src/ReducedEhclonForm.java
+++ src/ReducedEhclonForm.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class ReducedEhclonForm{
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //creating variables for operations
+        int row = 4; // store the no of rows in matrix
+        int column = 5; // store the no of columns in matrix
+
+        double [][] matrix = new double[row][column]; //2D array to manage/interact with rows and columns
+
+        //Taking the input of matrix of (3x4) from user
+        System.out.println("Enter the Elements of Matrix: ");
+        for(int i=1; i < row; i++){
+            for(int j=1; j < column; j++){
+                matrix[i][j] = input.nextInt();
+            } // inner for
+        } // outer for
 
     }   // main method
 } //main class
