4e13fae61fa0bd7607ae02501f6f41e832199aa2
usaid-khan-chandio <usiadkhanchandio@gmail.com>
2024-05-09 13:38:53 +0500

    Added Feature checking R1 1st element is not 1

--- src/StepsAndLogic.java
+++ src/StepsAndLogic.java
@@ -37,4 +37,31 @@ public class StepsAndLogic {
             } //outer for
         } //outer if
     }
+    static void if_R1_1st_Element_is_Zero_0(double[][] matrix){
+        /* Checking the R1's First Element is 0 if it is
+         * then we are add +1 to whole Row */
+        if (matrix[1][1] == 0) { //checking the R1's 1st Element is 0
+            //if its 0 then we are adding +1 in R1 CN
+            matrix[1][1] = matrix[1][1] + 1;
+            matrix[1][2] = matrix[1][2] + 1;
+            matrix[1][3] = matrix[1][3] + 1;
+            matrix[1][4] = matrix[1][4] + 1;
+        } //outer if
     }
+    static void if_R1_1st_Element_is_Pos_But_Not_1(double[][] matrix, int row, int column){
+        //Step-3
+        /* Checking If (R1's -- 1st Element is not 1, and it's greater than 0)
+         * means R1's 1st element is not a negative number then we perform the
+         * Elementary Row operations. Here we Divide the R1's -- 1st element to
+         * with complete R1 The logic is like this.
+         * (R1 (1/N) where N = The element that you want to change.In our case
+         * N = R1 & C1 means R1's 1st element because we want R1 & C1 1st Element as 1 */
+        if(matrix[1][1] > 0 && matrix[1][1] !=1) { //checking R1's 1st element is greater than 0 and not = 1
+            double divisor =  matrix[1][1];
+            for (int j = 1; j < column; j++) {
+                matrix[1][j] /= divisor;
+            } //inner for
+        } //outer for
+    } //outer if
+
+}
